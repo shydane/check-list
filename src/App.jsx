02597c7ccb7bf5fe938c33b066c37cc9bd5d13e6
Ihsanuddin <ihsanuddin.sy@gmail.com>
2025-01-07 05:52:08 +0700

    feat(App.jsx):koreksi dan penyempurnaan component Stats

--- src/App.jsx
+++ src/App.jsx
@@ -142,15 +142,25 @@ function Item({ note, onDeleteItem, onToggleDone }) {
 }
 
 function Stats({ notes }) {
+  if (notes.length === 0) {
+    return (
+      <footer className="bg-slate-700 text-slate-300 text-lg text-center font-semibold w-full p-3">
+        <span>📝 yuk mulai bikin catatan</span>
+      </footer>
+    );
+  }
+
   const totalNotes = notes.length;
   const completedNotes = notes.filter((note) => note.done).length;
   const completionRate = totalNotes ? (completedNotes / totalNotes) * 100 : 0;
 
   return (
-    <footer className="bg-slate-700 text-slate-300 text-base text-center font-semibold w-full p-3">
+    <footer className="bg-slate-700 text-slate-300 text-lg text-center font-semibold w-full p-3">
       <span>
-        kamu punya {totalNotes} catatan dan {completedNotes} note yang sudah di
-        checklist ({completionRate.toFixed(2)}%){" "}
+        {completionRate === 100
+          ? "✅ kamu sudah melakukan semua catatan!"
+          : `kamu punya ${totalNotes} catatan dan ${completedNotes} note yang sudah di
+          checklist (${completionRate}%)`}
       </span>
     </footer>
   );
